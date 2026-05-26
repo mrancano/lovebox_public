a6dab1a21626da2a2245b251f28160adabce5290
mrancano <mateo.arganza@gmail.com>
2026-05-26 18:24:07 +0200

    rwt

--- listener_v2.py
+++ listener_v2.py
@@ -212,7 +212,7 @@ def setup_gpio():
     else:
         print("Warning: gpiozero not found. Mocking button functionality.")
     if AudioController is not None:
-        state.audio = AudioController(player="mpv --audio-device=alsa/hw:0,0")
+        state.audio = AudioController()
 
 def main():
     global application
